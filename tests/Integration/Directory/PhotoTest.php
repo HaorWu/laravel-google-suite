9477127668f172e274f8f47885f71eb70147e4a5
Osmell Caicedo <correo.oele@gmail.com>
2020-07-25 15:11:52 -0500

    ✅ add tests for photo class

---
+++ tests/Integration/Directory/PhotoTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace oeleco\GoogleSuite\Tests\Integration\Directory;
+
+use Mockery as m;
+
+use oeleco\GoogleSuite\Tests\TestCase;
+use oeleco\GoogleSuite\Directory\Photo\Photo;
+use oeleco\GoogleSuite\Directory\Account\Account;
+
+class PhotoTest extends TestCase
+{
+    /** @var \oeleco\GoogleSuite\Directory\Photo\Photo */
+    protected $photo;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->photo = new Photo;
+    }
+
+    public function getImage()
+    {
+        $url = "https://placekitten.com/200/300";
+        $img = file_get_contents($url);
+        list($width, $height, $type) = getimagesizefromstring($img);
+
+        return [
+            'data'   => base64_encode($img),
+            'height' => $height,
+            'width'  => $width,
+            'type'   => $type
+        ];
+    }
+
+    /** @test */
+    public function it_can_update_the_photo_for_user_account()
+    {
+        $account = Account::create([
+            'email'     => "test@".$this->hosted_domain,
+            'firstName' => 'testname',
+            'lastName'  => 'testlastname',
+            'password'  => "password"
+        ]);
+
+        $photo = Photo::findOrNew("test@". $this->hosted_domain);
+        $photo->update($this->image);
+        $this->assertEquals($this->image['data'], $photo->googlePhoto['photoData']);
+
+        $photo->delete();
+        $account->delete();
+    }
+
+}
