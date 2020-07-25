00ad2410f8d85015f69e7299b9da5298177226cb
Osmell Caicedo <correo.oele@gmail.com>
2020-07-25 13:34:08 -0500

    ✅ add member tests

---
+++ tests/Integration/Directory/MemberTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace oeleco\GoogleSuite\Tests\Integration\Directory;
+
+use DateTime;
+use Mockery as m;
+
+use oeleco\GoogleSuite\Tests\TestCase;
+use oeleco\GoogleSuite\Directory\Group\Group;
+use oeleco\GoogleSuite\Directory\Member\Member;
+use oeleco\GoogleSuite\Directory\Account\Account;
+
+class MemberTest extends TestCase
+{
+
+    /** @var \oeleco\GoogleSuite\Directory\Member\Member */
+    protected $group;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->member = new Member;
+    }
+
+    /** @test */
+    public function it_can_add_a_member_to_google_group()
+    {
+        $group = Group::create([
+            'name'  => "grouptest",
+            'email' => "grouptest@". $this->hosted_domain
+        ]);
+
+        $account = Account::create([
+            'firstName' => $this->faker->firstName(),
+            'lastName'  => $this->faker->lastname,
+            'password'  => "password",
+            'email'     => "test@". $this->hosted_domain
+        ]);
+
+        $member = Member::create($group->id, [
+                'email' => $account->email,
+                'role' => 'MEMBER'
+        ]);
+
+        $this->assertTrue($member->id != null);
+
+        $member->delete();
+        $account->delete();
+        $group->delete();
+    }
+
+}
